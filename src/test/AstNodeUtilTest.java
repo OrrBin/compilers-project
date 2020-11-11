fcc4b0a4bf3d4559a916ea80ccabb1e0bbb3d3dd
ozzafar <ozzafar@mail.tau.ac.il>
2020-11-11 17:25:12 +0200

    AstNodeUtilTest - split to 2 test classes

--- src/test/AstNodeUtilTest.java
+++ src/test/AstNodeUtilTest.java
@@ -1,60 +1,33 @@
 package test;
 
-import ast.*;
+import ast.AstXMLSerializer;
+import ast.Program;
 import solution.AstNodeUtil;
 import solution.SymbolTablesManager;
-import solution.VariableType;
 import solution.symbol_table.SymbolTableInitVisitor;
 
 import java.io.File;
 
-public class AstNodeUtilTest {
+public abstract class AstNodeUtilTest {
+    protected static SymbolTablesManager symbolTablesManager = new SymbolTablesManager();
+    protected static AstNodeUtil astNodeUtil;
+    protected static Program prog;
 
-    public static SymbolTablesManager symbolTablesManager = new SymbolTablesManager();
-    public static AstNodeUtil astNodeUtil;
-    public static Program prog;
-    public static final String xmlFileName = "examples/ex1/field.java.xml";
+    private static String xmlFileName = "examples/ex1/field.java.xml";
 
     // TODO check if can add junit dependency
 
-    public static void init(){
+    public AstNodeUtilTest(String xmlFileName) {
+        this.xmlFileName = xmlFileName;
+    }
+
+    public void init(){
         astNodeUtil = new AstNodeUtil(symbolTablesManager);
 
         AstXMLSerializer xmlSerializer = new AstXMLSerializer();
         prog = xmlSerializer.deserialize(new File(xmlFileName));
 
         prog.accept(new SymbolTableInitVisitor(symbolTablesManager));
-    }
-
-    public static void getExtendingClassesReturnEmptyListTest(){
-        ClassDecl exampleClass = prog.classDecls().get(0);
-        assert astNodeUtil.getExtendingClasses(exampleClass).size() == 0;
-    }
-
-    public static void findVariableTypeTest() throws Exception {
-        ClassDecl exampleClass = prog.classDecls().get(0);
-        VarDecl xField = exampleClass.fields().get(0);
-        VariableType type = null;
-
-        type = astNodeUtil.findVariableType(xField);
-        assert type == VariableType.FIELD;
-
-        MethodDecl otherMethod = exampleClass.methoddecls().get(1);
-        VarDecl xLocal = otherMethod.vardecls().get(0);
-        type = astNodeUtil.findVariableType(xLocal);
-        assert type == VariableType.LOCAL;
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        AstNodeUtilTest.init();
-
-        AstNodeUtilTest.getExtendingClassesReturnEmptyListTest();
-        System.out.println("Test1 passed");
-        AstNodeUtilTest.findVariableTypeTest();
-        System.out.println("Test2 passed");
-        System.out.println("All AstNodeUtilTest's tests passed");
-
     }
 
 }
---
+++ src/test/AstNodeUtilTest_BS.java
@@ -0,0 +1,44 @@
+package test;
+
+import ast.*;
+
+import java.util.List;
+
+public class AstNodeUtilTest_BS extends AstNodeUtilTest{
+
+    private static final String xmlFileName = "examples/ast/TreeVisitor.java.xml";
+
+    public AstNodeUtilTest_BS(String xmlFileName) {
+        super(xmlFileName);
+    }
+
+    public void getExtendingClassesTest(){
+        ClassDecl visitorClass = prog.classDecls().get(2);
+        List<ClassDecl> extendingClasses = astNodeUtil.getExtendingClasses(visitorClass);
+        assert extendingClasses.size() == 1;
+        assert extendingClasses.get(0).name().equals("MyVisitor");
+    }
+
+    public void getClassTest(){
+        ClassDecl visitorClass = prog.classDecls().get(2);
+        MethodDecl visitorMethod = visitorClass.methoddecls().get(0);
+        VarDecl nti = visitorMethod.vardecls().get(0);
+        assert astNodeUtil.getClassDeclaration(visitorClass).equals(visitorClass);
+        assert astNodeUtil.getClassDeclaration(visitorMethod).equals(visitorClass);
+        assert astNodeUtil.getClassDeclaration(nti).equals(visitorClass);
+    }
+
+    public static void main(String[] args) {
+        AstNodeUtilTest_BS astNodeUtilTestBS = new AstNodeUtilTest_BS(xmlFileName);
+        astNodeUtilTestBS.init();
+
+        astNodeUtilTestBS.getExtendingClassesTest();
+        System.out.println("Test1 passed");
+        astNodeUtilTestBS.getClassTest();
+        System.out.println("Test2 passed");
+
+        System.out.println("All tests passed");
+
+    }
+
+}

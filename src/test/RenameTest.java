13cfe764d263943e1a4960123535740dfb0c5e54
Orr Benyamini <orrb@amazon.com>
2020-11-14 23:10:09 +0200

    Added color to test prints

--- src/test/RenameTest.java
+++ src/test/RenameTest.java
@@ -19,6 +19,15 @@ import java.util.stream.Collectors;
 
 //  unmarshal rename var x 10 y  examples/ex1/field_renamed.java.xml examples/ex1/field.java.xml examples/ex1/field_renamed_res.java.xml
 public class RenameTest {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) {
 //        var filename = "examples/ex1/method.java.xml";
@@ -116,7 +125,12 @@ public class RenameTest {
                         }
                     }
                     String whitespace = " ".repeat(maxLineLength - lineLength);
-                    System.out.println(String.format("%02d", i+1) + ". " + originalLines.get(i) + whitespace + "   |   " + renamedLines.get(i));
+                    if(originalLines.get(i).equals(renamedLines.get(i))) {
+                        System.out.println(String.format("%03d", i + 1) + ". " + originalLines.get(i) + whitespace + "   |   " + renamedLines.get(i));
+                    } else {
+                        System.out.println(String.format("%03d", i + 1) + ". " + ANSI_RED + originalLines.get(i) + whitespace +
+                                ANSI_RESET + "   |   " + ANSI_GREEN + renamedLines.get(i) + ANSI_RESET);
+                    }
                 }
 
 

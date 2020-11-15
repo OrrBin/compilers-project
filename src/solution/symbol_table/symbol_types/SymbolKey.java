d89a4704c1276eb1d4c733caccd8f38d4780c787
Orr Benyamini <orrb@amazon.com>
2020-11-15 21:35:18 +0200

    Added color to test prints

---
+++ src/solution/symbol_table/symbol_types/SymbolKey.java
@@ -0,0 +1,35 @@
+package solution.symbol_table.symbol_types;
+
+import java.util.Objects;
+
+public class SymbolKey {
+
+    public String name;
+    public SymbolKeyType type;
+
+    public SymbolKey(String name, SymbolKeyType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        SymbolKey symbolKey = (SymbolKey) o;
+        return Objects.equals(name, symbolKey.name) &&
+                type == symbolKey.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
+
+    enum SymbolKeyType {
+        VAR,
+        METHOD
+    }
+}
